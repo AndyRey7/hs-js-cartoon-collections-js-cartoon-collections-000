610f4925e36776a70fe7598ba6bb8f5874b09f59
andy reyes <11710reyes@gmail.com>
2018-12-01 04:36:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function dwarfRollCall(dwarves) {
+  var array = [];
+  var i = 0;
+  for (i = 0; i < dwarves.length; i++) {
+   array.push(`${i+1} ${dwarves}`) 
+  }
+  return array;
 }
 
 function summonCaptainPlanet(planeteerCalls){
